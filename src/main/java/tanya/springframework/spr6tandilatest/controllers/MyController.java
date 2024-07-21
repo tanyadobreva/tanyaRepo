5993c3f320c1a4bae3314118489ceaa33dde400f
Tanya Dobreva <1tanya.ivanova@gmail.com>
2024-07-21 22:26:32 +0300

    and now to mwin

--- src/main/java/tanya/springframework/spr6tandilatest/controllers/MyController.java
+++ src/main/java/tanya/springframework/spr6tandilatest/controllers/MyController.java
@@ -9,6 +9,6 @@ public class MyController {
     public String sayHello(){
         System.out.println("Hello tan");
 
-        return "hellooo you999 nowdddPULLiiMAAAINnnpub";
+        return "hellooo you999 nowdddPULLiiMAAAINnnpubkljlkjlkjlkjlk";
     }
 }
