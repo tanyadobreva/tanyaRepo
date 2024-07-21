7f2fd89b9c1e04789937492022696fc90f72d9f3
Tanya Dobreva <1tanya.ivanova@gmail.com>
2024-07-21 22:17:25 +0300

    testit

---
+++ src/main/java/tanya/springframework/spr6tandilatest/Spr6TanDiLatestApplication.java
@@ -0,0 +1,16 @@
+package tanya.springframework.spr6tandilatest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class Spr6TanDiLatestApplication {
+
+    public static void main(String[] args) {
+
+
+        ApplicationContext ctx = SpringApplication.run(Spr6TanDiLatestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tanya/springframework/spr6tandilatest/Spr6TanDiLatestApplicationTests.java
@@ -0,0 +1,13 @@
+package tanya.springframework.spr6tandilatest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spr6TanDiLatestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
