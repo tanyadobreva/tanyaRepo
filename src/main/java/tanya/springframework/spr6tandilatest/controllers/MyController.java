139ef307d5dbcab8d716b1137a2dedc4c37c21bb
Tanya Dobreva <1tanya.ivanova@gmail.com>
2024-07-21 21:49:23 +0300

    testit

---
+++ src/main/java/tanya/springframework/spr6tandilatest/controllers/MyController.java
@@ -0,0 +1,14 @@
+package tanya.springframework.spr6tandilatest.controllers;
+
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyController {
+
+    public String sayHello(){
+        System.out.println("Hello tan");
+
+        return "hellooo you";
+    }
+}
